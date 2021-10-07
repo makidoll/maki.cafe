d625a5ad111cadfb4dc91ee73c7a4edb6d31f5e9
Maki <mxmcube@gmail.com>
2021-10-06 22:58:40 -0500

    Added plausible

---
+++ scripts/download-plausible.mjs
@@ -0,0 +1,24 @@
+import fetch from "node-fetch";
+import * as path from "path";
+import * as fs from "fs";
+
+const __dirname = path.dirname(import.meta.url.replace("file:///", ""));
+
+const filename = "66c2e6a0f3149850fd26.js"; // half of sha1 "plausible.js"
+const url = "https://plausible.tivolicloud.com/js/plausible.js";
+
+(async () => {
+	const res = await fetch(url);
+	if (!res.ok) {
+		console.error("Failed to fetch Plausible js file");
+		process.exit(1);
+	}
+
+	let js = await res.text();
+	js = js.replace(/\[src\*="'\+[a-z]\+'"\]/, `[src*="${"/" + filename}"]`);
+
+	const pathToJs = path.resolve(__dirname, "../src/" + filename);
+	fs.writeFileSync(pathToJs, js);
+
+	console.log("Successfully written " + pathToJs);
+})();
