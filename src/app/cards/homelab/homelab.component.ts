7fc2a0e1dc39de75ad528a21ec9528e5e8225f61
Maki <mxmcube@gmail.com>
2021-10-25 01:49:49 -0500

    Updated homelab

--- src/app/cards/homelab/homelab.component.ts
+++ src/app/cards/homelab/homelab.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { shuffleArray } from "../../utils";
 
 @Component({
 	selector: "app-homelab",
@@ -6,13 +7,17 @@ import { Component, OnInit } from "@angular/core";
 	styleUrls: ["./homelab.component.scss"],
 })
 export class HomelabComponent implements OnInit {
+	// personalYeti = shuffleArray([
 	personalYeti = [
 		["Lanyard", "https://lanyard.cutelab.space"],
 		["Nitter", "https://nitter.cutelab.space"],
 		["Bibliogram", "https://bibliogram.cutelab.space"],
+		["Nitter", "https://nitter.cutelab.space"],
 		["Meli", "https://github.com/getmeli/meli"],
 		["Pihole", "https://pi-hole.net"],
 		["Emby", "https://emby.media/"],
+		["Jellyfin", "https://jellyfin.org/"],
+		["Plex", "https://www.plex.tv/"],
 		["RSS Bridge", "https://github.com/RSS-Bridge/rss-bridge"],
 		["Mastodon", "https://mastodon.cutelab.space"],
 		["FreshRSS", "https://freshrss.org"],
@@ -27,6 +32,20 @@ export class HomelabComponent implements OnInit {
 		["Deluge", "https://deluge-torrent.org"],
 		["Traefik", "https://traefik.io/traefik"],
 		["Librespeed", "https://speedtest.cutelab.space"],
+		["InvoiceNinja", "https://www.invoiceninja.com"],
+		[
+			"Speedtest Tracker",
+			"https://github.com/henrywhitaker3/Speedtest-Tracker",
+		],
+	];
+	// ]);
+
+	tivoliYeti = [
+		["Website and API", "https://tivolicloud.com"],
+		["Ghost blog", "https://blog.tivolicloud.com"],
+		["GitLab and runner", "https://git.tivolicloud.com/tivolicloud"],
+		["Plausible analytics", "https://plausible.io"],
+		["Cardano node", "https://cardano.org"],
 	];
 
 	constructor() {}
