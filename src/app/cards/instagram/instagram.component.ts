fd1ffd10ccd2cc5dcaab3dcedf340a382df7c002
Maki <mxmcube@gmail.com>
2021-12-17 17:56:45 -0600

    Instagram fixes and added info

--- src/app/cards/instagram/instagram.component.ts
+++ src/app/cards/instagram/instagram.component.ts
@@ -16,27 +16,34 @@ interface Post {
 })
 export class InstagramComponent implements OnInit {
 	readonly bibliogramUrl = config.api.bibliogram;
+	readonly maxPosts = 9;
 
 	config = config;
 
 	@Input() username: string;
 
 	posts: Post[] = [];
+	totalPosts = -1;
+	followers = -1;
+	following = -1;
 
 	constructor() {}
 
 	ngOnInit(): void {
 		if (isScullyRunning()) return;
 
-		this.getPosts(this.username);
+		this.getData(this.username);
 	}
 
-	async getPosts(username: string) {
+	async getData(username: string) {
 		const res = await fetch(this.bibliogramUrl + "/u/" + username);
 		const htmlStr = await res.text();
 		const $ = cheerio.load(htmlStr);
 
+		// posts
 		$("a.sized-link").each((i, linkEl) => {
+			if (i > this.maxPosts - 1) return;
+
 			const link = "https://www.instagram.com" + linkEl.attribs.href;
 			const imageEl = linkEl.firstChild as cheerio.Element;
 			const alt = imageEl.attribs.alt;
@@ -47,12 +54,19 @@ export class InstagramComponent implements OnInit {
 					.map(src => src.trim())
 					.find(src => src.endsWith("150w"))
 					.split(" ")[0];
+
 			this.posts.push({
 				alt,
 				link,
 				imageUrl,
 			});
 		});
+
+		// info
+		const infoDivs = $(".profile-counter .count");
+		this.totalPosts = parseInt(infoDivs[0].attribs["data-numberformat"]);
+		this.followers = parseInt(infoDivs[1].attribs["data-numberformat"]);
+		this.following = parseInt(infoDivs[2].attribs["data-numberformat"]);
 	}
 
 	/*
