ba9b237b0d1e57793ae2c82e456b04785cc205d2
Maki <mxmcube@gmail.com>
2022-11-01 17:45:04 -0500

    Reset dancing letters animation when changes

--- components/ui/DancingLetters.tsx
+++ components/ui/DancingLetters.tsx
@@ -1,14 +1,25 @@
 import { chakra } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import styles from "./DancingLetters.module.scss";
 
 export default function DancingLetters(props: { children: string }) {
 	const letters = props.children ?? "";
+
+	const [enabled, setEnabled] = useState(true);
+
+	useEffect(() => {
+		setEnabled(false);
+		setTimeout(() => {
+			setEnabled(true);
+		}, 100);
+	}, [letters]);
+
 	return (
 		<>
 			{letters.split("").map((letter, i) => (
 				<chakra.span
 					key={i}
-					className={styles.letter}
+					className={enabled ? styles.letter : ""}
 					style={{
 						animationDelay: (letters.length - i) * -100 + "ms",
 						display: letter == " " ? "initial" : "",
