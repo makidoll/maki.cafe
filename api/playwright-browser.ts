cb3b58bb1ee3d26484ff78794027735542881cb0
Maki <mxmcube@gmail.com>
2023-05-04 15:54:35 -0400

    Add todo

--- api/playwright-browser.ts
+++ api/playwright-browser.ts
@@ -1,6 +1,8 @@
 import { GlobalRef } from "../utils/global-ref";
 import { Browser, firefox } from "playwright";
 
+// TODO: browser could launch multiple times, need something like a replay subject
+
 const browser = new GlobalRef<Browser>("playwright.browser");
 
 export async function getBrowser() {
