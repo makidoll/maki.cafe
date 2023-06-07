f300e8b13ff4e7cfe825dd28258bafa400326876
Maki <mxmcube@gmail.com>
2023-06-07 07:13:12 -0400

    Add margin to home cards when in mobile

--- components/ui/home-card/HomeCard.tsx
+++ components/ui/home-card/HomeCard.tsx
@@ -3,7 +3,12 @@ import { Box, Flex } from "@chakra-ui/react";
 export default function HomeCard(props: { children?: any }) {
 	const opacity = 0.2;
 	return (
-		<Flex width={450} alignItems="center" justifyContent="center">
+		<Flex
+			width={450}
+			alignItems="center"
+			justifyContent="center"
+			marginBottom={[8, 8, 8, 0]}
+		>
 			<Box
 				// boxShadow="0 0 64px #0000001a, 0 0 32px #0000001a"
 				boxShadow={`0 0 96px rgba(0,0,0,${opacity}), 0 0 64px rgba(0,0,0,${opacity}), 0 0 32px rgba(0,0,0,${opacity})`}
