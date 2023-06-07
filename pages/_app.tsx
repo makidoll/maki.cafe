d6342f24784004bf9abd2cdf6229e98170f47f3f
Maki <mxmcube@gmail.com>
2023-06-06 22:40:28 -0400

    Update fav icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -68,28 +68,27 @@ function MyApp({ Component, pageProps }: AppProps) {
 				<link
 					rel="apple-touch-icon"
 					sizes="180x180"
-					href="/icons/apple-touch-icon.png"
+					href="/apple-touch-icon.png"
 				/>
 				<link
 					rel="icon"
 					type="image/png"
 					sizes="32x32"
-					href="/icons/favicon-32x32.png"
+					href="/favicon-32x32.png"
 				/>
 				<link
 					rel="icon"
 					type="image/png"
 					sizes="16x16"
-					href="/icons/favicon-16x16.png"
+					href="/favicon-16x16.png"
 				/>
-				<link rel="manifest" href="/icons/site.webmanifest" />
+				<link rel="manifest" href="/site.webmanifest" />
 				<link rel="shortcut icon" href="/favicon.ico" />
-				<meta name="msapplication-TileColor" content="#e91e63" />
+				<meta name="msapplication-TileColor" content="#1d1f21" />
 				<meta
 					name="msapplication-config"
-					content="/icons/browserconfig.xml"
+					content="/browserconfig.xml"
 				/>
-				<meta name="theme-color" content="#ffffff" />
 				<meta name="theme-color" content="#ffffff" />
 				<meta name="description" content={description} />
 				<meta property="og:url" content={url} />
