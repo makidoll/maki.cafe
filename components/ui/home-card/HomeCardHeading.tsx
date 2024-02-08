3567ac489bd923b69438f5dd2c7d2e985826b429
Maki <maki@hotmilk.space>
2024-02-08 14:24:52 -0500

    Fix home card heading

--- components/ui/home-card/HomeCardHeading.tsx
+++ components/ui/home-card/HomeCardHeading.tsx
@@ -1,55 +1,65 @@
-import { Flex, Heading, HeadingProps, Link } from "@chakra-ui/react";
+import {
+	ChakraComponent,
+	Flex,
+	FlexProps,
+	Heading,
+	HeadingProps,
+	Link,
+	LinkProps,
+	chakra,
+} from "@chakra-ui/react";
 import { IconType } from "react-icons";
 import { kgAlwaysAGoodTime } from "../../../fonts/fonts";
 import { config } from "../../../utils/config";
 
 export default function HomeCardHeading(
-	props: HeadingProps & { icon?: IconType; href?: string },
+	props: FlexProps & { icon?: IconType; href?: string },
 ) {
 	const mt = -2;
 
 	const Icon = props.icon;
 	const href = props.href;
+	const children = props.children;
 
 	props = { ...props };
 	delete props.icon;
 	delete props.href;
+	delete props.children;
 
-	let H = (
-		<Heading
-			fontFamily={kgAlwaysAGoodTime.style.fontFamily}
-			fontWeight={400}
-			fontSize={"24px"}
-			textAlign={"center"}
-			textTransform={"lowercase"}
-			mt={Icon ? 0 : mt}
-			mr={Icon ? 2 : 0}
-			{...(props as any)}
-		/>
-	);
+	const linkProps = {
+		href,
+		color: "#fff",
+		transition: config.styles.hoverTransition,
+		transformOrigin: "center",
+		_hover: {
+			transform: "scale(1.05)",
+		},
+	};
 
-	if (Icon) {
-		H = (
-			<Flex flexDir={"row"} mt={mt}>
-				{H}
-				<Icon size={24} color="#fff" />
-			</Flex>
-		);
-	}
-
-	if (href) {
-		H = (
-			<Link
-				color="#fff"
-				href={href}
-				transition={config.styles.hoverTransition}
-				transformOrigin="center"
-				_hover={{ transform: "scale(1.05)" }}
+	return (
+		<Flex
+			flexDir={"row"}
+			width={"100%"}
+			mt={mt}
+			alignItems={"center"}
+			justifyContent={"center"}
+			as={href ? "a" : "div"}
+			{...(href ? linkProps : {})}
+			{...props}
+		>
+			{Icon ? <Icon size={24} color="#fff" /> : <></>}
+			<Heading
+				fontFamily={kgAlwaysAGoodTime.style.fontFamily}
+				fontWeight={400}
+				fontSize={"24px"}
+				textAlign={"center"}
+				textTransform={"lowercase"}
+				ml={Icon ? 2 : 0}
+				mt={0}
+				mb={0}
 			>
-				{H}
-			</Link>
-		);
-	}
-
-	return H;
+				{children}
+			</Heading>
+		</Flex>
+	);
 }
